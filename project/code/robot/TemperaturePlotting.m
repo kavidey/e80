d2505343d0599e28e484c871600c1964c370079c
madeleine <128001078+madeleieine@users.noreply.github.com>
2024-04-14 16:15:16 -0700

    Update TemperaturePlotting.m

--- project/code/robot/TemperaturePlotting.m
+++ project/code/robot/TemperaturePlotting.m
@@ -1,3 +1,6 @@
+% 2d array; 3 columns with x coords, y coords, and temperature vals
+
 data = ???
 % agregate data into 2d matrix
-heatmap(data); %plots heat map of temperature
+heatmap(data, 'x position', 'y position'); %plots heat map of temperature
+title = 'temperature accross sampling area'
