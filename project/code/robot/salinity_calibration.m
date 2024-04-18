46c6cbfeff0f81d1443cac66521600e39ae8297d
kavidey <kaviasher@gmail.com>
2024-04-17 23:19:24 -0700

    Add 3d salinity calibration

---
+++ project/code/robot/salinity_calibration.m
@@ -0,0 +1,41 @@
+% High Salinity Resolution Trials
+hs_waterMass = [81.1 55.4 55.6 69.1 38.0 56.6 58.4 60.6 89.7]; % [mL]
+hs_saltMass = [0.8 0.8 1.0 1.6 1.0 1.9 2.0 2.3 3.8]; % [g]
+hs_temp = 21.5 * ones(size(hs_waterMass)); % [C]
+hs_vout = [1.86 1.98 2.04 2.10 2.11 2.13 2.12 2.15 2.18]; % [V]
+hs_vin = [2.20 2.18 2.17 2.15 2.15 2.15 2.14 2.14 2.14]; % [V]
+
+hs_salinity = 1000 * hs_saltMass ./ (hs_saltMass + hs_waterMass) % [ppt]
+hs_gain = hs_vout ./ hs_vin;
+
+% High Temp Resolution Trials
+ht_waterMass = [81.1 81.1 81.1 81.1 81.1 56.6 56.6 56.6 56.6 89.7 89.7 89.7 89.7]; % [mL]
+ht_saltMass = [0.8 0.8 0.8 0.8 0.8 1.9 1.9 1.9 1.9 3.8 3.8 3.8 3.8]; % [g]
+ht_temp = [26.8 26.1 39.3 35.3 31.7 41.8 36.2 30.4 25.8 40.2 34.8 29.6 25.6]; % [C]
+ht_vout = [1.88 1.75 2.04 1.98 1.96 2.23 2.20 2.19 2.17 2.27 2.23 2.40 2.23]; % [V]
+ht_vin  = [1.53 1.52 1.50 1.51 1.51 1.48 1.48 1.48 1.48 1.47 1.47 1.47 1.47]; % [V]
+
+ht_salinity = 1000 * ht_saltMass ./ (ht_saltMass + ht_waterMass) % [ppt]
+ht_gain = ht_vout ./ ht_vin;
+
+
+salinity = [hs_salinity ht_salinity];
+temp = [hs_temp ht_temp];
+gain = [hs_gain ht_gain];
+%%
+figure(1)
+clf
+hold on
+view(3)
+scatter3(salinity, temp, gain, 'filled', 'SizeData', 100)
+[X, Y] = meshgrid(linspace(min(salinity), max(salinity), 100), linspace(min(temp), max(temp), 100));
+Z = griddata(salinity, temp, gain, X, Y, "cubic");
+surf(X, Y, Z);
+
+xlabel("Salinity [ppt]")
+ylabel("Temp [C]")
+zlabel("Gain")
+hold off
+
+
+
