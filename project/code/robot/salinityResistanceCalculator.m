0e0569a6bec5feedc91eff91e3ad0b5acfbdb506
madeleine <128001078+madeleieine@users.noreply.github.com>
2024-04-17 22:59:22 -0700

    more temperature data etc

--- project/code/robot/salinityResistanceCalculator.m
+++ project/code/robot/salinityResistanceCalculator.m
@@ -1,13 +1,50 @@
-Vindiv = [1.59 2.20 1.54 1.53 1.53 2.0]';
+% all voltages are peak-to-peak
+Vindiv = [1.52
+1.5
+1.50
+1.49
+1.49
+1.48
+1.48
+1.48
+1.48];
 Rin1 = 10070
 Rin2 = 14760
 Vin = Vindiv/(Rin2/(Rin1+Rin2));
 
-Vout = [1.67 1.93 1.95 1.96 1.94 1.54]';
+Vout = [1.86
+1.98
+2.04
+2.10
+2.11
+2.13
+2.12
+2.15
+2.18];
 R2 = 67;
-Rprobe = (Vin-Vout)*R2./Vout
-salinity = [10.67 32.75 31.25 33.93 34.69 40.85]';
+Rprobe = (Vin-Vout)*R2./Vout;
+
+waterMasses = [81.1
+55.4
+55.6
+69.1
+38.0
+56.6
+58.4
+60.6
+89.7];
+saltMasses = [0.8
+0.8
+1.0
+1.6
+1.0
+1.9
+2.0
+2.3
+3.8];
+salinity = saltMasses./(saltMasses+waterMasses);
 
 scatter(salinity, Rprobe, "filled")
 xlabel('Salinity (ppt)')
 ylabel('Rprobe (\Omega)')
+
