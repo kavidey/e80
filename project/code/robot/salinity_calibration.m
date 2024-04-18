42a85447f0ad54ace98ebed6688266a306ff4cd9
madeleine <128001078+madeleieine@users.noreply.github.com>
2024-04-18 15:47:48 -0700

    polynomial fit

--- project/code/robot/salinity_calibration.m
+++ project/code/robot/salinity_calibration.m
@@ -1,3 +1,4 @@
+clc
 % High Salinity Resolution Trials
 hs_waterMass = [81.1 55.4 55.6 69.1 38.0 56.6 58.4 60.6 89.7]; % [mL]
 hs_saltMass = [0.8 0.8 1.0 1.6 1.0 1.9 2.0 2.3 3.8]; % [g]
@@ -5,7 +6,7 @@ hs_temp = 21.5 * ones(size(hs_waterMass)); % [C]
 hs_vout = [1.86 1.98 2.04 2.10 2.11 2.13 2.12 2.15 2.18]; % [V]
 hs_vin = [2.20 2.18 2.17 2.15 2.15 2.15 2.14 2.14 2.14]; % [V]
 
-hs_salinity = 1000 * hs_saltMass ./ (hs_saltMass + hs_waterMass) % [ppt]
+hs_salinity = 1000 * hs_saltMass ./ (hs_saltMass + hs_waterMass); % [ppt]
 hs_gain = hs_vout ./ hs_vin;
 
 % High Temp Resolution Trials
@@ -15,7 +16,7 @@ ht_temp = [26.8 26.1 39.3 35.3 31.7 41.8 36.2 30.4 25.8 40.2 34.8 29.6 25.6]; % 
 ht_vout = [1.88 1.75 2.04 1.98 1.96 2.23 2.20 2.19 2.17 2.27 2.23 2.40 2.23]; % [V]
 ht_vin  = [1.53 1.52 1.50 1.51 1.51 1.48 1.48 1.48 1.48 1.47 1.47 1.47 1.47]; % [V]
 
-ht_salinity = 1000 * ht_saltMass ./ (ht_saltMass + ht_waterMass) % [ppt]
+ht_salinity = 1000 * ht_saltMass ./ (ht_saltMass + ht_waterMass); % [ppt]
 ht_gain = ht_vout ./ ht_vin;
 
 % Low temp resolution trials
@@ -31,7 +32,7 @@ lt_gain = lt_vout ./ lt_vin;
 salinity = [ht_salinity lt_salinity];
 temp = [ht_temp lt_temp];
 gain = [ht_gain lt_gain];
-%%
+%% griddata fit
 figure(1)
 clf
 hold on
@@ -46,5 +47,34 @@ ylabel("Temp [C]")
 zlabel("Gain")
 hold off
 
+%% trying to do polynomial fit here
+% 2nd/3rd degree
+figure(2)
+clf
+subplot(1, 2, 1);
+hold on
+view(3)
+scatter3(salinity, temp, gain, 'filled', 'SizeData', 100);
+[x,y,z] = meshgrid(linspace(min(salinity), max(salinity), 100), linspace(min(temp), max(temp), 100), linspace(min(gain), max(gain), 100));
+[Zpoly23, gof23] = fit([salinity' temp'], gain', 'poly23')
+plot(Zpoly23);
+title("poly23")
+xlabel("Salinity [ppt]")
+ylabel("Temp [C]")
+zlabel("Gain")
+
+
+% 3rd/4th degree
+subplot(1, 2, 2);
+view(3)
+scatter3(salinity, temp, gain, 'filled', 'SizeData', 100);
+[x,y,z] = meshgrid(linspace(min(salinity), max(salinity), 100), linspace(min(temp), max(temp), 100), linspace(min(gain), max(gain), 100));
+[Zpoly34, gof34] = fit([salinity' temp'], gain', 'poly34')
+plot(Zpoly34);
+title("poly34")
+xlabel("Salinity [ppt]")
+ylabel("Temp [C]")
+zlabel("Gain")
+hold off
 
 
