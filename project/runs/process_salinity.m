5eae969a616bdf29889d4afe9924bde7d6b0ccfa
mbrandt22 <157071578+mbrandt22@users.noreply.github.com>
2024-04-23 21:01:08 -0700

    Update process_salinity.m

--- project/runs/process_salinity.m
+++ project/runs/process_salinity.m
@@ -1,4 +1,8 @@
-function [salinity, time] = process_salinity(burst_file_name, rows_per_sample)
+% Miranda Brandt
+% Kavi Dey 
+% Salinity Processing Code 
+
+function [salinity, time] = process_salinity(burst_file_name, rows_per_sample, temp)
 T = readtable(burst_file_name);
 data = table2array(T);
 
@@ -8,12 +12,42 @@ teensy_conversion = 3.3/2^10;
 
 time = [];
 salinity = [];
+
+load('salinityfits.mat')
+
 for i = 0:(num_samples-1)
-    time = [time data(i*rows_per_sample+1, 1) * 1e-6];
-    % THIS DOES NOT ACTUALLY CALCULATE SALINITY, IT JUST TELLS US IF THERE IS VALID DATA
+    %create sample time array 
+    time = [time data(i*rows_per_sample+1, 1) * 1e-6]; %Fix Time 
+    samp_time = data(i*rows_per_sample+1, :) * 1e-6;
+    % VALID DATA CHECK
     vin = data(i*rows_per_sample+2, :) * teensy_conversion;
     vout = data(i*rows_per_sample+3, :) * teensy_conversion;
-    salinity = [salinity max(vin)];
+    %Finding the gain
+    diff = samp_time(1) - samp_time(2);
+    fs = 1/diff;
+    N = length(vin);
+    f0 = fs/N; %Fundamental Frequency
+    X = fft(vin)/N;
+    X2 = fft(vout)/N;
+
+    true_vin = max(X);
+    true_vout = max(X2);
+    %L1 vin L2 vout new_t is time 
+    Gain = true_vout/true_vin;
+    salinity = [salinity Zpoly23(Gain, temp)];
 end
 
 end
+
+% %Plot vin and vout
+% plot(f, abs(X), 'b');
+% hold on 
+% plot(f, abs(X2), 'r');
+% hold off
+% legend('vin', 'vout')
+% xlabel('Frequency (Hz)');
+% ylabel('Values (Voltage)');
+% xlim([0,inf]);
+% 
+% temps = 16;
+% salinity = Zpoly23(Gain, temps)
